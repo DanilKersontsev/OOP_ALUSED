7bdedb3847e6f6ae34d7fb92e3ef884df8992244
Danil Kersontsev <danil.kersontsev@khk.ee>
2022-01-13 09:42:04 +0200

    Ül 4 - Restoraan

---
+++ praks2/restoraanid.py
@@ -0,0 +1,22 @@
+from praks2.restoraan import Restoraan
+
+minu_restoraan = Restoraan()
+minu_restoraan.restoraani_nimi = " Kolm tilli"
+minu_restoraan.soogi_tyyp = " Maitsvat toitu"
+
+minu_restoraan.kirjelda_restoraan()
+minu_restoraan.ava_restoraan()
+
+s_restoraan = Restoraan()
+s_restoraan.restoraani_nimi = " Gervini restoraan"
+s_restoraan.soogi_tyyp = " Maitsvat toitu"
+
+s_restoraan.kirjelda_restoraan()
+s_restoraan.ava_restoraan()
+
+v_restoraan = Restoraan()
+v_restoraan.restoraani_nimi = " Viktori restoran"
+v_restoraan.soogi_tyyp = " Maitvat toitu"
+
+v_restoraan.kirjelda_restoraan()
+v_restoraan.ava_restoraan()
