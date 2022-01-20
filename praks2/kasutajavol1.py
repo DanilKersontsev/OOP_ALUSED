aae6ec0e8d2667a6f7487f9676b6b39a494c1b21
Danil Kersontsev <danil.kersontsev@khk.ee>
2022-01-20 09:17:55 +0200

    Ül 7 - Kasutajad konstruktori abil

---
+++ praks2/kasutajavol1.py
@@ -0,0 +1,17 @@
+from praks2.kasutaja1 import Kasutaja
+
+esimene = Kasutaja(" Danil", "Kersontsev", "17", "danil.kersontsev@voco.ee")
+
+esimene.kirjelda_kasutaja()
+esimene.tervita_kasutaja()
+
+
+teine = Kasutaja(" Gervin", "Luide", "17", "gervin.luide@voco.ee")
+
+teine.kirjelda_kasutaja()
+teine.tervita_kasutaja()
+
+kolmas = Kasutaja(" Viktor", "Lumiste", "16", "viktor.lumiste@voco.ee")
+
+kolmas.kirjelda_kasutaja()
+kolmas.tervita_kasutaja()
