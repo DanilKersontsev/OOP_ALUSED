efbf33c5b597a820161d69fd69e055e859aecc88
Danil Kersontsev <danil.kersontsev@khk.ee>
2022-02-09 09:50:24 +0200

    Ülesanne 17 - Pank

---
+++ praks6/konto.py
@@ -0,0 +1,19 @@
+class Konto():
+    def __init__(self, nimi, saldo):
+        self.nimi = nimi
+        self.saldo = saldo
+
+    def deposiit(self, kogus):
+        self.saldo += kogus
+
+    def ylekanne(self, amount):
+        if self.saldo <= amount:
+            self.saldo -= amount
+        else:
+            self.saldo = 0
+    def naita_saldo(self):
+        return self.saldo
+    def naita_nimi(self):
+        return self.nimi
+
+
