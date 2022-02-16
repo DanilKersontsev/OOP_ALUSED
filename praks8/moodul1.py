72f61e9c14048d4bb2025cff1c900534807d2e6c
Danil Kersontsev <danil.kersontsev@khk.ee>
2022-02-16 09:00:50 +0200

    Ülesanne 20 - Administraator ja privileegid moodulite abil

---
+++ praks8/moodul1.py
@@ -0,0 +1,11 @@
+class Privileegid():
+    def __init__(self, privileegid):
+        self.privileegid = []
+        self.privileegid.append(privileegid)
+    def naita_privileegid(self):
+        print(self.privileegid)
+
+class Admin():
+    privileegid = "lubatud lisada kasutajad, lubatud eemaldada kasutajad, lubatud blokeerida kasutajad"
+    def naita_privileegid(self):
+        print(self.privileegid)
---
+++ praks8/test.py
@@ -0,0 +1,10 @@
+from administraator import *
+from moodul1 import *
+
+kasutaja = Kasutaja("Danil", "Kersontsev", "17", "danil.kersontsev@voco.ee")
+
+kasutaja.kirjelda_kasutaja()
+kasutaja.tervita_kasutaja()
+
+admin2 = Privileegid("lubatud lisada kasutajad")
+admin2.naita_privileegid()
